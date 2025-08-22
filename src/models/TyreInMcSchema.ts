467b5e5c3dc677b697c4128d9307afa926eddfa6
Surya Prakash Singh <suryaprakash3209@gmail.com>
2025-08-22 18:22:29 +0530

    mongoose package version update

--- src/models/TyreInMcSchema.ts
+++ src/models/TyreInMcSchema.ts
@@ -1,7 +1,5 @@
-import type { Document, Model, Schema as SchemaType } from 'mongoose';
-import mongoose from 'mongoose';
-
-const Schema = mongoose.Schema
+import * as mongoose from "mongoose";
+import { Schema, Document, Model } from "mongoose";
 
 export interface ITyreMc extends Document {
   billDate: string;
@@ -16,7 +14,7 @@ export interface ITyreMc extends Document {
   withoutGstPay: string;
 }
 
-const TyreMcSchema: SchemaType<ITyreMc> = new Schema(
+const TyreMcSchema: Schema<ITyreMc> = new Schema(
   {
     billDate: { type: String, required: true, trim: true },
     fitmentDate: { type: String, default: "STOCK", trim: true },
